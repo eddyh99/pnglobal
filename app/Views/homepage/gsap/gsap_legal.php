b9b0184657a8c4d3a3fe324a520415b2c9df04b5
yanyiik99 <aripramana574@gmail.com>
2024-09-13 21:07:10 +0800

    merge with live in github

--- app/Views/homepage/gsap/gsap_legal.php
+++ app/Views/homepage/gsap/gsap_legal.php
@@ -1,15 +1,15 @@
 <script>
     gsap.registerPlugin(ScrollTrigger);
     
-    gsap.from(".banner-text-video", {
-        x: "-100%",
-        duration: 1,
-        scrollTrigger: {
-            trigger: "banner-text-video",
-            start: "top 90%",
-            end: "top 50%",
-        }
-    })
+    // gsap.from(".banner-text-video", {
+    //     x: "-100%",
+    //     duration: 1,
+    //     scrollTrigger: {
+    //         trigger: "banner-text-video",
+    //         start: "top 90%",
+    //         end: "top 50%",
+    //     }
+    // })
 
     
     gsap.to("#g-legalshortdesc-title .line-animation", {
