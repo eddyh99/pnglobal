8cc43541169c0a768edd749c02e1f55c1b2f3fef
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-01-27 16:02:44 +0800

    feat: update session files and enhance navbar layout with new button styles

--- app/Views/homepage/layout/header.php
+++ app/Views/homepage/layout/header.php
@@ -1,10 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= $title?></title>
+    <title><?= $title ?></title>
 
     <!-- Favicons -->
     <link href="<?= BASE_URL ?>assets/img/logo.png" rel="icon">
@@ -20,20 +21,21 @@
 
     <!-- Telephone Code -->
     <link href="<?= BASE_URL ?>assets/libs/intl-tel-input-master/build/css/intlTelInput.css" rel="stylesheet">
-    
+
 
     <!-- Stripe -->
     <script src="https://js.stripe.com/v3/"></script>
-    
+
     <!-- Custom CSS -->
     <link rel="stylesheet" href="<?= BASE_URL ?>assets/style/css/main.css">
 </head>
+
 <body>
     <!-- Start of Navbar -->
-    <nav class="navbar navbar-expand-xxl <?= @$navoption ? 'position-relative' : ''?>">
+    <nav class="navbar navbar-expand-xxl <?= @$navoption ? 'position-relative' : '' ?>">
         <div class="container">
-            <a class="navbar-logo" href="<?= BASE_URL?>">
-                <img class="logo" src="<?= BASE_URL?>assets/img/new-logo.png" alt="logo pnglobal">
+            <a class="navbar-logo" href="<?= BASE_URL ?>">
+                <img class="logo" src="<?= BASE_URL ?>assets/img/new-logo.png" alt="logo pnglobal">
             </a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <div class="openbtn">
@@ -44,26 +46,29 @@
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav ms-auto">
-                    <li class="nav-item me-0 me-xl-2">
-                        <!-- <a class="nav-link" aria-current="page" href="<?php echo BASE_URL . "homepage/service?service=" . base64_encode("satoshi_signal")?>">Satoshi Signal</a> -->
-                        <a class="nav-link" aria-current="page" href="<?=BASE_URL?>?type=satoshi">Satoshi Signal</a>
+                    <li class="nav-item me-0 me-xl-4 <?= (current_url() === BASE_URL('index.php/homepage/hotdeals')) ? 'active' : '' ?>">
+                        <a class="nav-link" translate="no" aria-current="page" href="<?= BASE_URL ?>homepage/hotdeals">HOT <br> DEAL</a>
                     </li>
                     <li class="nav-item me-0 me-xl-2">
-                        <a class="nav-link" aria-current="page" href="<?=BASE_URL?>?type=productservice">Product and Service</a>
+                        <a class="nav-link" aria-current="page" href="<?= BASE_URL ?>?type=productservice">SERVICES <br> INFORMATION</a>
                     </li>
                     <li class="nav-item me-0 me-xl-2">
-                        <a class="nav-link" aria-current="page" href="<?=BASE_URL?>?type=training">Training</a>
+                        <a class="nav-link" aria-current="page" href="<?= BASE_URL ?>?type=training">TRAINING <br> COURSES</a>
                     </li>
                     <li class="nav-item me-0 me-xl-2">
-                        <a class="nav-link" href="<?=BASE_URL?>?type=service">Complementary Service</a>
+                        <a class="nav-link" href="<?= BASE_URL ?>?type=service">FINANCIAL <br> BLOG</a>
                     </li>
-                    <li class="nav-item me-0 me-lg-3 d-flex align-items-center justify-content-center">
-                        <a class="btn-navbar-referral" href="<?= BASE_URL ?>">Referral Login</a>
-                    </li>
-                    <li class="nav-item mt-2 mt-lg-0 d-flex align-items-center justify-content-center">
+                    <!-- <li class="nav-item me-0 me-lg-3 d-flex align-items-center justify-content-center">
+                        <a class="btn-navbar-referral" href="<?= BASE_URL ?>">LOGIN WITH REFERRAL</a>
+                    </li> -->
+                    <!-- <li class="nav-item mt-2 mt-lg-0 d-flex align-items-center justify-content-center">
                         <a class="btn-navbar-contactform" href="<?= BASE_URL ?>homepage/contactreferral">Get Referral</a>
-                    </li>
+                    </li> -->
                 </ul>
+                <div class="nav-buttons">
+                    <a class="btn-navbar-referral" href="<?= BASE_URL ?>">LOGIN WITH REFERRAL</a>
+                    <!-- <a class="btn-navbar-contactform" href="<?= BASE_URL ?>homepage/contactreferral">Get Referral</a> -->
+                </div>
             </div>
         </div>
     </nav>
