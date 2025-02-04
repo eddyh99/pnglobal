ff7df7481dbbaa9aaa31c0faaa578f6f48740655
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-02-04 10:22:34 +0800

    feat: add Satoshi Signal banner with video background and styling enhancements

---
+++ app/Views/homepage/service/satoshi_signal.php
@@ -0,0 +1,24 @@
+<div class="page-wrapper">
+    <!-- Start of Banner -->
+    <div class="banner-satoshi">
+        <div class="wrapper-banner-satoshi">
+            <video autoplay="" preload="" muted="" loop="" playsinline="">
+                <source src="<?= BASE_URL ?>assets/vid/bg-satoshi-signal.mp4" type="video/webm">
+            </video>
+            <div class="logo-banner">
+                <div class="text-wrapper">
+                    <p class="logo-text-banner">
+                        <span class="goal-text">
+                            THE REVOLUTIONARY Satoshi Signal App
+                        </span>
+                    </p>
+                    <p class="discover-text">
+                        Privileged access to the best signals, directly on your device. <br>
+                        Follow Expert Brokers, Earn Risk-Free, and Get Guaranteed Profits
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- End of Banner -->
+</div>
