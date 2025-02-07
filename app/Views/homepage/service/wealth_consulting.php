67ee68520c884bb9ab6b86fe24646b99d3411703
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-02-07 10:33:46 +0800

    feat: add wealth consulting section with layout, styling, and content integration

---
+++ app/Views/homepage/service/wealth_consulting.php
@@ -0,0 +1,27 @@
+<div class="page-wrapper">
+    <section class="wealth-consulting">
+        <div class="container-fluid p-0">
+            <div class="row g-0">
+                <!-- Left Column with Image -->
+                <div class="col-12 col-lg-6">
+                    <div class="wc-image">
+                        <img src="<?= BASE_URL ?>assets/img/ic_wealth_consulting.png" alt="Wealth Consulting" class="img-fluid">
+                    </div>
+                </div>
+                <!-- Right Column with Text Content -->
+                <div class="col-12 col-lg-6">
+                    <div class="wc-content">
+                        <h1 class="wc-title">Wealth Consulting</h1>
+                        <h2 class="wc-subtitle">Preserve and Grow Your Wealth</h2>
+                        <p class="wc-description">
+                            Secure your wealth from unforeseen risks and maximize your assets' efficiency. PN Global's consultants help you to identify hidden risks and optimize your asset management.
+                        </p>
+                        <button class="wc-button">
+                            Optimize Your <br> Wealth Strategy
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+</div>
