06a17d125054facf56c4e71781b6ed0c5da7c736
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-02-11 15:15:05 +0800

    feat: create free member

---
+++ app/Controllers/Godmode/Freemember.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Controllers\Godmode;
+
+use App\Controllers\BaseController;
+
+class Freemember extends BaseController
+{
+    public function __construct()
+    {
+        // $session = session();
+        // if(!$session->has('logged_user')){
+        //     header("Location: ". BASE_URL . 'godmode/auth/signin');
+        //     exit();
+        // }
+        
+        // if ($_SESSION["logged_user"]->role!='admin'){
+        //     header('HTTP/1.0 403 Forbidden');
+        //     exit();
+        // }
+
+    }
+    
+    public function index()
+    {
+        $mdata = [
+            'title'     => 'Free Member - ' . SATOSHITITLE,
+            'content'   => 'godmode/freemember/index',
+            'extra'     => 'godmode/freemember/js/_js_index',
+            'active_free'    => 'active active-menu'
+        ];
+
+        return view('godmode/layout/admin_wrapper', $mdata);
+    }
+
+    public function createfree()
+    {
+        // Validation Field
+        $rules = $this->validate([
+            'email' => [
+                'label' => 'Email',
+                'rules' => 'required|valid_email'
+            ],
+            'expired' => [
+                'label' => 'Free Member Expiration Date',
+                'rules' => 'required'
+            ],
+        ]);
+    
+        // Checking Validation
+        if (!$rules) {
+            session()->setFlashdata('error_validation', $this->validation->listErrors());
+            return redirect()->to(BASE_URL . 'godmode/freemember');
+        }
+    
+        // Init Data
+        $mdata = [
+            'email'   => htmlspecialchars($this->request->getVar('email')),
+            'upline'    => htmlspecialchars($this->request->getVar('upline')),
+            'expired' => htmlspecialchars($this->request->getVar('expired')),
+        ];
+    
+        
+    }
+
+
+    public function detailmember($email)
+    {
+
+        // Call Get Memeber By Email
+        $url = URLAPI . "/auth/getmember_byemail?email=".base64_decode($email);
+        $resultMember = satoshiAdmin($url)->result->message;
+
+        // Call Get Detail Referral
+        $url = URLAPI . "/v1/member/detailreferral?id=".$resultMember->id;
+        $resultReferral = satoshiAdmin($url)->result->message;
+
+        $mdata = [
+            'title'     => 'Detail Member - ' . SATOSHITITLE,
+            'content'   => 'godmode/freemember/detail_freemember',
+            'extra'     => 'godmode/freemember/js/_js_detailreferral',
+            'active_free'  => 'active',
+            'member'    => $resultMember,
+            'referral'  => $resultReferral,
+            'emailreferral' => base64_decode($email),
+        ];
+
+        return view('godmode/layout/admin_wrapper', $mdata);
+    }
+
+    public function upgrademember(){
+        // Init Data
+        $mdata = [
+            'email'    => $this->request->getVar('email'),
+            'expired'  => date_format(date_create($this->request->getVar('expired')),"Y-m-d"),
+        ];
+    }
+    
+    public function cancelfree($email){
+        $email  = base64_decode($email);
+    }
+    
+}
