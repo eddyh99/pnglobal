fa43a2c0bb763189d76048326bf1fe9e21efab06
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-02-25 17:43:48 +0800

    feat: Enhance payment request handling in godmode with API integration and DataTables configuration

--- app/Controllers/Godmode/Payment.php
+++ app/Controllers/Godmode/Payment.php
@@ -17,9 +17,9 @@ class Payment extends BaseController
         //     header('HTTP/1.0 403 Forbidden');
         //     exit();
         // }
-        
+
     }
-    
+
     public function index()
     {
         $mdata = [
@@ -31,13 +31,22 @@ class Payment extends BaseController
 
         return view('godmode/layout/admin_wrapper', $mdata);
     }
-    
-    public function get_requestpayment(){
+
+    public function get_requestpayment()
+    {
         // Call Endpoin Get Total Member
-        
+        $url = URLAPI . "/v1/withdraw/request_payment";
+        $response = satoshiAdmin($url);
+        $result = $response->result;
+
+        $data = [
+            'code' => $result->code,
+            'message' => $result->message
+        ];
+        return json_encode($data);
     }
 
-    public function detailpayment($id,$email)
+    public function detailpayment($id, $email)
     {
         // Call Get Detail Request
         // $url = URLAPI . "/v1/payment/detailpayment?email=".base64_decode($email)."&id=".$id;
@@ -62,19 +71,14 @@ class Payment extends BaseController
             'member_id'    => htmlspecialchars($this->request->getVar('member_id')),
             'reqid'  => htmlspecialchars($this->request->getVar('reqid')),
         ];
-
-        
-        
     }
-    
-    public function sendbonus(){
+
+    public function sendbonus()
+    {
         // Init Data
         $mdata = [
             'email'    => htmlspecialchars($this->request->getVar('email')),
             'amount'  => htmlspecialchars($this->request->getVar('amount')),
         ];
-        
-            
     }
-    
 }
