9c6a2dd6573bbab5c90967127777b8e496707bd1
I Putu Wira Budhi Guna Ariyasa <wirbud1134@gmail.com>
2025-03-18 18:08:51 +0800

    feat: Add Delete Message Functionality
    - Implement delete message button in the message index view
    - Add confirmation dialog using SweetAlert before deletion
    - Update JavaScript to handle message deletion via AJAX with user feedback

--- app/Views/godmode/message/js/_js_index.php
+++ app/Views/godmode/message/js/_js_index.php
@@ -24,6 +24,25 @@
         $("input[name='subject']").val(title);
         $("#summernote").summernote("code", message);
         $("#frm-message").attr("action", "<?= BASE_URL ?>godmode/message/editmessage/" + msgid);
+    });
 
+    $(document).on("click", ".delete-message", function(e) {
+        e.preventDefault();
+        let msgid = $(this).closest("tr").find(".msgid").val();
+
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "Message that has been deleted cannot be restored!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete!',
+            cancelButtonText: 'Cancel'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                window.location.href = "<?= BASE_URL ?>godmode/message/deletemessage/" + msgid;
+            }
+        });
     });
 </script>
