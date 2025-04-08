622f516de66a7669c6ea8212dda919fb3f52219e
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-04-08 10:39:57 +0700

    Update constant

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'codeigniter4/framework',
         'pretty_version' => 'dev-main',
         'version' => 'dev-main',
-        'reference' => '096467ff80039161ac0402ba01ebdf8c83252bfe',
+        'reference' => 'e0aa3a9c7c4b217cb4f9a2d6f3c82df48c69a0d2',
         'type' => 'project',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -31,7 +31,7 @@
         'codeigniter4/framework' => array(
             'pretty_version' => 'dev-main',
             'version' => 'dev-main',
-            'reference' => '096467ff80039161ac0402ba01ebdf8c83252bfe',
+            'reference' => 'e0aa3a9c7c4b217cb4f9a2d6f3c82df48c69a0d2',
             'type' => 'project',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
