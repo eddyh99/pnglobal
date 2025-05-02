9472fc58df36bd1dbb2f9fe04651b73aa40fe9d3
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-05-02 08:52:17 +0700

    compose

--- app/Views/course/member/message/compose.php
+++ app/Views/course/member/message/compose.php
@@ -1,6 +1,6 @@
 <div class="d-flex vh-100">
     <!-- friends -->
-         <!-- Sidebar dengan garis pembatas kanan -->
+    <!-- Sidebar dengan garis pembatas kanan -->
     <div id="sidebar" class="bg-black text-white" style="width: 220px;border-right: 4px solid #B48B3D;"">
         <ul class=" nav flex-column text-center">
         <li class="nav-item mb-2">
@@ -15,16 +15,24 @@
             </ul>
             <hr class="m-0" style="border: 1px solid #B48B3D;">
         </li>
-        <?php foreach($friends as $f): ?>
-        <li class="nav-item">
-            <a href="#" class="nav-link text-white px-4 py-3 subject" data-id="<?= $f['id'] ?>"><?= $f['nama'] ?></a>
-        </li>
+        <?php foreach ($friends as $f): ?>
+            <li class="nav-item">
+                <a href="#" class="nav-link text-white px-4 py-3 subject" data-id="<?= $f['id'] ?>"><?= $f['nama'] ?></a>
+            </li>
         <?php endforeach ?>
 
         </ul>
     </div>
     <!-- Konten utama -->
     <div class="flex-grow-1 p-4 text-center">
-        <h3>No content available.</h3>
+        <div class="container">
+            <h3 class="mb-5">Message</h3>
+            <!-- <button class="btn btn-primary mb-3" id="sendButton">Send message</button> -->
+            <div class="input-group mw-100 mb-0">
+                <span class="input-group-text text-white bg-primary" style="padding: 0.7rem;" id="basic-addon3">Subject</span>
+                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+            </div>
+            <textarea id="editor" class="w-100 form-control" style="min-height: 350px;" placeholder="Message..."></textarea>
+        </div>
     </div>
 </div>
