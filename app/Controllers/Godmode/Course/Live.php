13d66373c4b513e49de0c4a450e47d3255cb02bf
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-05-21 10:14:48 +0700

    create live

--- app/Controllers/Godmode/Course/Live.php
+++ app/Controllers/Godmode/Course/Live.php
@@ -7,13 +7,78 @@ use App\Controllers\BaseController;
 class Live extends BaseController
 {
     public function index() {
+
+        $url = URL_COURSE . "/v1/user/mentor";
+        $result = satoshiAdmin($url)->result;
         $mdata = [
             'title'     => 'Live - ' . NAMETITLE,
             'content'   => 'godmode/course/live/index',
             'extra'     => 'godmode/course/live/js/_js_index',
             'active_live'    => 'active active-menu',
+            'mentor'    => $result->message ?? []
         ];
 
         return view('godmode/course/layout/admin_wrapper', $mdata);
     }
+
+    public function store() {
+        $isValid = $this->validate([
+            'title' => [
+                'label' => 'Title Course',
+                'rules' => 'required',
+            ],
+            'mentor_id' => [
+                'label' => 'Mentor',
+                'rules' => 'required|integer',
+            ],
+            'start_date' => [
+                'label' => 'Start Date',
+                'rules' => 'required|valid_date',
+            ],
+            'time' => [
+                'label' => 'Time',
+                'rules' => 'required',
+            ],
+            'duration' => [
+                'label' => 'Duration',
+                'rules' => 'required',
+            ]
+        ]);
+        
+
+        // Checking Validation
+        if (!$isValid) {
+            session()->setFlashdata('failed', $this->validation->listErrors());
+            return redirect()->to(BASE_URL . 'godmode/course/live')->withInput();
+        }
+
+        $startDateTime = date(
+            'Y-m-d H:i:s',
+            strtotime($this->request->getVar('start_date') . ' ' . $this->request->getVar('time'))
+        );
+        
+        $mdata = [
+            'title'      => $this->request->getVar('title'),
+            'mentor_id'  => $this->request->getVar('mentor_id'),
+            'start_date' => $startDateTime,
+            'duration'   => $this->durationToMinutes($this->request->getVar('duration'))
+        ];
+
+        $response = satoshiAdmin(URL_COURSE . "/v1/live/store", json_encode($mdata));
+        $result = $response->result;
+    
+        if ($result->code != 201) {    
+            session()->setFlashdata('failed', $result->message);
+            return redirect()->to(BASE_URL . 'godmode/course/live')->withInput();
+        } 
+
+        session()->setFlashdata('success', $result->message);
+        return redirect()->to(BASE_URL . 'godmode/course/live');
+    }
+
+    private function durationToMinutes(string $duration): int {
+        list($hours, $minutes) = explode(':', $duration);
+        return ((int)$hours * 60) + (int)$minutes;
+    }
+    
 }
