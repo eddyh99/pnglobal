f67a0638cf1586b27d885e1f870d06c4f9362c22
Miftahus Surur Sutowo <62152669+miftahozuki@users.noreply.github.com>
2025-05-23 15:16:48 +0700

    active member hedge

--- app/Views/godmode/dashboard/js/_js_detailmember.php
+++ app/Views/godmode/dashboard/js/_js_detailmember.php
@@ -1,6 +1,9 @@
 <link rel="stylesheet" href="https://code.jquery.com/ui/1.14.1/themes/base/jquery-ui.css">
 <script src="https://code.jquery.com/ui/1.14.1/jquery-ui.js"></script>
 <script>
+
+    const url = new URL(window.location.href);
+    const tab = url.searchParams.get("tab");
     $("#btnref").on("click", function() {
         const walletInput = document.getElementById('refcode');
         walletInput.select(); // Select the text
@@ -21,73 +24,104 @@
         });
     }, 5000);
 
+    // $('#table_referralmember').DataTable({
+    //     "pageLength": 100,
+    //     "scrollX": true,
+    //     "ajax": {
+    //         "url": "<?= BASE_URL ?>godmode/dashboard/get_downline/" + $("#id").val(),
+    //         "type": "POST",
+    //         "dataSrc": function(data) {
+    //             return data.message;
+    //             console.log(data.message);
+    //         },
+    //         "data": function(d) {
+    //             d.product = tab;
+    //             // console.log(d.id_member);
+    //         },
+    //     },
+    //     "columns": [{
+    //             data: 'email'
+    //         },
+    //         {
+    //             data: null,
+    //             "mRender": function(data, type, full, meta) {
+    //                 if (full.status == 'active') {
+    //                     return `<div>
+    //                                 <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="8" height="8" rx="4" fill="#0E7304"/></svg>
+    //                                 Active
+    //                             </div>`;
+    //                 } else if (full.status == 'new') {
+    //                     return `<div>
+    //                                 <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="8" height="8" rx="4" fill="#7F7F7F"/></svg>
+    //                                 New
+    //                             </div>`;
+
+    //                 } else {
+    //                     return `<div>
+    //                                 <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="8" height="8" rx="4" fill="#FF0000"/></svg>
+    //                                 Inactive
+    //                             </div>`;
+    //                 }
+    //             }
+    //         },
+    //         {
+    //             data: null,
+    //             "mRender": function(data, type, full, meta) {
+    //                 if (!full.start_date || !full.end_date) {
+    //                     return `<div>
+    //                                 <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="8" height="8" rx="4" fill="#FF0000"/></svg>
+    //                                 Inactive
+    //                             </div>`;
+    //                 }
+
+    //                 var start = moment(full.start_date, "YYYY-MM-DD HH:mm:ss");
+    //                 var end = moment(full.end_date, "YYYY-MM-DD HH:mm:ss");
+
+    //                 if (!start.isValid() || !end.isValid()) {
+    //                     return `<div>
+    //                                 <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="8" height="8" rx="4" fill="#FF0000"/></svg>
+    //                                 Inactive
+    //                             </div>`;
+    //                 }
+
+    //                 var diffDays = end.diff(start, 'days');
+    //                 return diffDays + " days";
+    //             }
+    //         }
+    //     ]
+    // });
+
     $('#table_referralmember').DataTable({
-        "pageLength": 100,
+        "pageLength": 50,
         "scrollX": true,
+        "order": false,
         "ajax": {
-            "url": "<?= BASE_URL ?>godmode/dashboard/get_referralmember",
+            "url": "<?= BASE_URL ?>godmode/dashboard/get_downline/" + $("#id").val(),
             "type": "POST",
-            "dataSrc": function(data) {
-                return data.message;
-                console.log(data.message);
+            "dataSrc":function (data){
+                console.log(data);
+                return data;							
             },
             "data": function(d) {
-                d.id_member = $('#id').val();
-                console.log(d.id_member);
+                d.product = tab;
+                // console.log(d.id_member);
             },
         },
-        "columns": [{
-                data: 'email'
-            },
-            {
-                data: null,
-                "mRender": function(data, type, full, meta) {
-                    if (full.status == 'active') {
-                        return `<div>
-                                    <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="8" height="8" rx="4" fill="#0E7304"/></svg>
-                                    Active
-                                </div>`;
-                    } else if (full.status == 'new') {
-                        return `<div>
-                                    <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="8" height="8" rx="4" fill="#7F7F7F"/></svg>
-                                    New
-                                </div>`;
-
-                    } else {
-                        return `<div>
-                                    <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="8" height="8" rx="4" fill="#FF0000"/></svg>
-                                    Inactive
-                                </div>`;
-                    }
-                }
-            },
-            {
-                data: null,
-                "mRender": function(data, type, full, meta) {
-                    if (!full.start_date || !full.end_date) {
-                        return `<div>
-                                    <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="8" height="8" rx="4" fill="#FF0000"/></svg>
-                                    Inactive
-                                </div>`;
-                    }
-
-                    var start = moment(full.start_date, "YYYY-MM-DD HH:mm:ss");
-                    var end = moment(full.end_date, "YYYY-MM-DD HH:mm:ss");
-
-                    if (!start.isValid() || !end.isValid()) {
-                        return `<div>
-                                    <svg width="8" height="8" viewBox="0 0 8 8" fill="none" xmlns="http://www.w3.org/2000/svg"><rect width="8" height="8" rx="4" fill="#FF0000"/></svg>
-                                    Inactive
-                                </div>`;
-                    }
-
-                    var diffDays = end.diff(start, 'days');
-                    return diffDays + " days";
-                }
-            }
-        ]
+        "columns": [
+            { data: 'email'},
+            { data: 'status'},
+            // { 
+            //     data: null, 
+            //     "mRender": function(data, type, full, meta) {
+            //         var subscription='';
+            //         if (parseInt(full.day)>0){
+            //             subscription = full.day + "days until "+full.end_date;
+            //         }
+            //         return subscription;
+            //     } 
+            // },
+        ],
     });
-
     <?php if ($tab === 'satoshi-signal') { ?>
         $('#table_referralmember_satoshi').DataTable({
             "pageLength": 50,
