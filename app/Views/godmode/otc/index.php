304c623e828beb5949a03d12cbacb49934caba2f
yanevannn <yanevan03@gmail.com>
2025-08-19 12:37:03 +0800

    Update: integration to api calculator otc

--- app/Views/godmode/otc/index.php
+++ app/Views/godmode/otc/index.php
@@ -1,33 +1,83 @@
+<?php if (!empty(session('success'))) { ?>
+    <div class="alert alert-success fade show position-absolute" style="top: 1rem; right: 1rem; width: 30%; z-index: 99999;" role="alert">
+        <div class="iq-alert-icon">
+            <i class="ri-information-line"></i>
+        </div>
+        <div class="iq-alert-text text-black">
+            <?= session('success') ?>
+        </div>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <i class="ri-close-line text-black"></i>
+        </button>
+    </div>
+<?php } ?>
+
+<?php if (!empty(session('failed'))) { ?>
+    <div class="alert alert-danger fade show position-absolute" style="top: 1rem; right: 1rem; width: 30%; z-index: 99999;" role="alert">
+        <div class="iq-alert-icon">
+            <i class="ri-information-line"></i>
+        </div>
+        <div class="iq-alert-text text-black">
+            <?= session('failed') ?>
+        </div>
+        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+            <i class="ri-close-line text-black"></i>
+        </button>
+    </div>
+<?php } ?>
 <!-- Page Content  -->
 <div class="content-page">
     <div class="container-fluid">
         <div class="row content-body">
             <div class="col-lg-12">
                 <div class="send-signals">
-                    <div class="title-signal-preview d-flex justify-content-between align-items-center">
-                        <h4>Calculator OTC</h4>
-                    </div>
-                    <div class="main-send-signal d-flex flex-column align-items-center justify-content-center">
-                        <div class="row w-100">
-                            <div class="form-addreferral col-8 mx-auto">
-                                <div class="wrapper-addreferral">
-                                    <label for="amountBtc">Amount BTC</label>
-                                    <input type="number" id="amountBtc" name="amountBtc" class="form-control" placeholder="0.001" step="any">
-                                </div>
-                                <div class="wrapper-addreferral">
-                                    <label for="buyPrice">Buy Price</label>
-                                    <input type="number" id="buyPrice" name="buyPrice" class="form-control" placeholder="0" step="any">
-                                </div>
-                                <div class="wrapper-addreferral">
-                                    <label for="sellPrice">Sell Price</label>
-                                    <input type="number" id="sellPrice" name="sellPrice" class="form-control" placeholder="0" step="any">
-                                </div>
-                                <div class="wrapper-addreferral d-flex justify-content-center mt-4">
-                                    <button type="button" id="calculateBtn" class="btn btn-primary">Calculate</button>
+                    <form action="" method="post">
+                        <div class="title-signal-preview d-flex justify-content-between align-items-center">
+                            <h4>Calculator OTC</h4>
+                        </div>
+                        <div class="main-send-signal d-flex flex-column align-items-center justify-content-center">
+                            <div class="row w-100">
+                                <div class="form-addreferral col-8 mx-auto">
+                                    <div class="wrapper-addreferral">
+                                        <label for="amount_btc">Amount BTC</label>
+                                        <div class="d-flex">
+                                            <input type="number" id="amount_btc" name="amount_btc" class="form-control" placeholder="0.001" step="any">
+                                            <label class="d-flex align-items-center gap-1 ml-2">
+                                                <input type="checkbox" name="lock_amount_btc" value="1">
+                                                <p class="mb-0">🔒</p>
+                                            </label>
+
+                                        </div>
+                                    </div>
+                                    <div class="wrapper-addreferral">
+                                        <label for="buy_price">Buy Price</label>
+                                        <div class="d-flex">
+                                            <input type="number" id="buy_price" name="buy_price" class="form-control" placeholder="0" step="any">
+                                            <label class="d-flex align-items-center gap-1 ml-2">
+                                                <input type="checkbox" name="lock_buy_price" value="1">
+                                                <p class="mb-0">🔒</p>
+                                            </label>
+
+                                        </div>
+                                    </div>
+                                    <div class="wrapper-addreferral">
+                                        <label for="sell_price">Sell Price</label>
+                                        <div class="d-flex">
+                                            <input type="number" id="sell_price" name="sell_price" class="form-control" placeholder="0" step="any">
+                                            <label class="d-flex align-items-center gap-1 ml-2">
+                                                <input type="checkbox" name="lock_sell_price" value="1">
+                                                <p class="mb-0">🔒</p>
+                                            </label>
+
+                                        </div>
+                                    </div>
+                                    <div class="wrapper-addreferral d-flex justify-content-center mt-4">
+                                        <button type="submit" id="calculateBtn" class="btn btn-primary">Calculate</button>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
             <!-- Bagian hasil perhitungan, disembunyikan secara default -->
