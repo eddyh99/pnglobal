beb3ec4b020d2e36c90aea32fe337f63a68b8fe6
yanevannn <yanevan03@gmail.com>
2025-08-22 12:26:16 +0800

    update:  calculations hedgefund data table

--- app/Views/homepage/hedgefund/index.php
+++ app/Views/homepage/hedgefund/index.php
@@ -8,6 +8,38 @@
                 <div class="col-lg-12">
                     <div class="container-fluid">
                         <div class="row dash-statistics">
+                            <!-- <div class="col-12 col-sm-6 col-lg-3 mb-2">
+                                <a class="d-block h-75">
+                                    <div class="iq-card h-100">
+                                        <div class="iq-card-body">
+                                            <div class="d-flex flex-column justify-content-center align-items-start">
+                                                <div>
+                                                    <h5 class="text-black">Total Op Fondo %</h5>
+                                                </div>
+                                                <div class="mt-3 text-center mx-auto">
+                                                    <h2 id="totalOpFondo" class="text-black fw-bold text-center">Loading...</h2>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                            <div class="col-12 col-sm-6 col-lg-3 mb-2">
+                                <a class="d-block h-75">
+                                    <div class="iq-card h-100">
+                                        <div class="iq-card-body">
+                                            <div class="d-flex flex-column justify-content-center align-items-start">
+                                                <div>
+                                                    <h5 class="text-black">Total Net Stor CLi %</h5>
+                                                </div>
+                                                <div class="mt-3 text-center mx-auto">
+                                                    <h2 id="totalNetStorCli" class="text-black fw-bold text-center">Loading...</h2>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </a>
+                            </div> -->
                             <div class="col-12 col-sm-6 col-lg-3 mb-2">
                                 <a class="d-block h-75">
                                     <div class="iq-card h-100">
@@ -59,6 +91,10 @@
                             <tr class="text-center">
                                 <th>Buy Price</th>
                                 <th>Sell Price</th>
+                                <!-- <th>Comm Buy</th>
+                                <th>Comm Sell</th>
+                                <th>Net</th>
+                                <th>Op Fondo %</th> -->
                                 <th>Net Historical Customer %</th>
                             </tr>
                         </thead>
