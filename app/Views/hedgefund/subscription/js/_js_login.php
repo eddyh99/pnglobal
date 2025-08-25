556c73dbd0a79f75effec29bb2c15545c9fbc4d6
yanevannn <yanevan03@gmail.com>
2025-08-25 15:34:39 +0800

    update: add password visibility toggle for Hedge member login

--- app/Views/hedgefund/subscription/js/_js_login.php
+++ app/Views/hedgefund/subscription/js/_js_login.php
@@ -5,34 +5,16 @@
         });
     }, 5000);
 
-    $(document).ready(function() {
-        $('#togglePassword').on('click', function() {
+    $('#togglePassword').on('click', function() {
+        const passwordField = $('#password');
+        const passwordFieldType = passwordField.attr('type');
 
-            const passwordField = $('#password');
-            const passwordFieldType = passwordField.attr('type');
-
-            // Toggle between text and password field type
-            if (passwordFieldType === 'password') {
-                passwordField.attr('type', 'text');
-                $(this).removeClass('la-eye').addClass('la-low-vision'); // Switch icon to "eye-slash"
-            } else {
-                passwordField.attr('type', 'password');
-                $(this).removeClass('la-low-vision').addClass('la-eye'); // Switch icon back to "eye"
-            }
-        })
-        $('#toggleConf').on('click', function() {
-
-            const passwordField = $('#confpassword');
-            const passwordFieldType = passwordField.attr('type');
-
-            // Toggle between text and password field type
-            if (passwordFieldType === 'password') {
-                passwordField.attr('type', 'text');
-                $(this).removeClass('la-eye').addClass('la-low-vision'); // Switch icon to "eye-slash"
-            } else {
-                passwordField.attr('type', 'password');
-                $(this).removeClass('la-low-vision').addClass('la-eye'); // Switch icon back to "eye"
-            }
-        })
-    })
+        if (passwordFieldType === 'password') {
+            passwordField.attr('type', 'text');
+            $(this).removeClass('fa-eye').addClass('fa-eye-slash');
+        } else {
+            passwordField.attr('type', 'password');
+            $(this).removeClass('fa-eye-slash').addClass('fa-eye'); 
+        }
+    });
 </script>
